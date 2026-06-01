3eceaaba80dca3888b47d39018434ed8da42b5cd
Saint <146729423+WhereIU@users.noreply.github.com>
2026-06-02 01:39:47 +0300

    app/config/settings/prod.py add comments

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,11 +1,15 @@
 from .base import *
 
-DEBUG = False
+# MODE
+DEBUG=1
 
+# SECURITY
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# SESSIONS
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
+# STATIC
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
